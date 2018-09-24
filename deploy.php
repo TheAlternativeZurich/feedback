05c49cf1f044926383838bfaafe906090d05b1b7
Florian Moser <products@famoser.ch>
2018-09-24 16:08:35 +0200

    correcting deploy shared folder

--- deploy.php
+++ deploy.php
@@ -19,7 +19,7 @@ set('var_dir', 'var');
 // Configuration
 set('repository', ' https://github.com/TheAlternativeZurich/feedback.git');
 set('shared_files', array_merge(get('shared_files'), ['var/data.sqlite']));
-set('shared_dirs', array_merge(get('shared_dirs'), ['public/upload']));
+set('shared_dirs', array_merge(get('shared_dirs'), ['public/templates']));
 set('symfony_env_file', '.env');
 set('composer_options', '{{composer_action}} --verbose --prefer-dist --no-progress --no-interaction --no-dev --optimize-autoloader --no-scripts');
 set('env_file_path', '.env');
