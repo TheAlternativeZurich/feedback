5da7b8b6acc7a894e710e4b3a02ab7bb09906f2e
Florian Moser <famoser@users.noreply.github.com>
2019-12-29 17:36:37 +0100

    Update deploy.php

--- deploy.php
+++ deploy.php
@@ -55,6 +55,7 @@ task('deploy:refresh_symlink', function () {
     run('killall -9 php-cgi'); //kill all php processes so symlink is refreshed
 })->desc('Refreshing symlink');
 
+// use PROD env & no interaction
 set('bin/console', function () {
     return parse('{{bin/php}} {{release_path}}/bin/console --no-interaction --env=PROD');
 });
