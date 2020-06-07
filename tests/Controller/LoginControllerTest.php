2b251c9c5ae110a105f0f13948891dc6ed1406ad
Florian Moser <git@famoser.ch>
2020-06-07 14:22:10 +0200

    add phpunit as dependency

--- tests/Controller/LoginControllerTest.php
+++ tests/Controller/LoginControllerTest.php
@@ -27,25 +27,6 @@ use Symfony\Component\HttpFoundation\Response;
 class LoginControllerTest extends WebTestCase
 {
     /**
-     * PHPUnit's data providers allow to execute the same tests repeated times
-     * using a different set of data each time.
-     * See https://symfony.com/doc/current/cookbook/form/unit_testing.html#testing-against-different-sets-of-data.
-     *
-     * @dataProvider getPublicUrls
-     */
-    public function testPublicUrls($url)
-    {
-        $client = static::createClient();
-        $client->request('GET', $url);
-
-        $this->assertSame(
-            Response::HTTP_OK,
-            $client->getResponse()->getStatusCode(),
-            sprintf('The %s public URL loads correctly.', $url)
-        );
-    }
-
-    /**
      * The application contains a lot of secure URLs which shouldn't be
      * publicly accessible. This tests ensures that whenever a user tries to
      * access one of those pages, a redirection to the login form is performed.
